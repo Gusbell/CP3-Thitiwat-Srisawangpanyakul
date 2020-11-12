f9de6b41abb48b19c591ae187b0041b965791d4b
Gusbell <Gusbell@users.noreply.github.com>
2020-11-13 01:27:10 +0700

    Exercise4

---
+++ Exercise4_Thitiwat_S.py
@@ -0,0 +1,11 @@
+FunEng = 89
+GenBus = 81
+IntroCom = 90
+ComPro = 70.59
+
+print("---Your Score---")
+print("Foundation English : %d" %FunEng)
+print("General Business : %d" %GenBus)
+print("Introduction to Computer Systems : %d" %IntroCom)
+print("Computer Programming : %.2f" %ComPro)
+
