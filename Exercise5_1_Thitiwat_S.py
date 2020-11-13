1908bd12a3743652de1a3e0155b956aaed042310
Gusbell <Gusbell@users.noreply.github.com>
2020-11-13 16:03:50 +0700

    Exercise5

---
+++ Exercise5_1_Thitiwat_S.py
@@ -0,0 +1,6 @@
+a = 16
+b = 8
+print("%d + %d = " %(a,b), a+b)
+print("%d - %d = " %(a,b), a-b)
+print("%d * %d = " %(a,b), a*b)
+print("%d / %d = " %(a,b), int(a/b))
---
+++ Exercise5_2_Thitiwat_S.py
@@ -0,0 +1,4 @@
+s = int(input("ระยะทาง(Km): "))
+t = int(input("เวลา(Hr): "))
+v = s/t
+print("ความเร็ว= ", v , "km/hr")
