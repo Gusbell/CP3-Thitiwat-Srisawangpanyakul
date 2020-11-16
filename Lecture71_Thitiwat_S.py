1d910b0ff9039cd461663a6af6327ce35fa853f4
Gusbell <Gusbell@users.noreply.github.com>
2020-11-16 21:24:25 +0700

    Lecture71

---
+++ Lecture71_Thitiwat_S.py
@@ -0,0 +1,25 @@
+menuList = []
+priceList = []
+
+def showBill():
+    print("---- My Food ----")
+    for number in range(len(menuList)):
+        print(menuList[number], priceList[number])
+
+def showTotal():
+    total = 0
+    print("---- Total ----")
+    for number in priceList:
+        total += int(number)
+    print("Total is %d Bath" %total)
+
+while True:
+    menuName = input("Plese Enter Menu :")
+    if(menuName.lower() == "exit"):
+        break
+    else:
+        menuPrice = input("Price :")
+        menuList.append(menuName)
+        priceList.append(menuPrice)
+showBill()
+showTotal()
